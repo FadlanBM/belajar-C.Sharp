5439250f9f6733374c7d2d0d953a8bb8a430872e
FadlanBM <fadlankurahan2@gmail.com>
2022-11-22 22:38:01 +0700

    upload data

--- belajarEdit_update_data/Form1.cs
+++ belajarEdit_update_data/Form1.cs
@@ -7,14 +7,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace belajarEdit_update_data
 {
     public partial class Form1 : Form
     {
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+
+        koneksi konn = new koneksi();
+
+        void cleardata()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        void tampildata() {
+            SqlConnection con = konn.getconn();
+            try
+            {
+                con.Open();
+                cmd = new SqlCommand("select * from tbsiswa", con);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "tbsiswa");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "tbsiswa";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception G)
+            {
+                MessageBox.Show(G.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tampildata();
+            cleardata();
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "")
+            {
+                MessageBox.Show("Data Belum lengkap");
+            }
+            else {
+                SqlConnection con = konn.getconn();
+                try
+                {
+                    cmd = new SqlCommand("insert into tbsiswa values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "')", con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("input data berhasi");
+                    tampildata();
+                    cleardata();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.ToString());
+
+                }
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ belajarEdit_update_data/koneksi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace belajarEdit_update_data
+{
+    internal class koneksi
+    {
+        public SqlConnection getconn()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString="Data source=WIN-4T7LHOS9DNI; initial catalog=dbsiswa; integrated security=true";
+            return conn;
+        }
+    }
+}
